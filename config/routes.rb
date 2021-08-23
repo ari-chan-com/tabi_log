7f632ae040131fee81c5a555f3076299d21f5738
linda <81902355+ari-chan-com@users.noreply.github.com>
2021-08-23 14:00:09 +0900

    routesファイル更新(post#list)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'post/list'
+  get 'post/list' =>"post#list"
   get '/' => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about' => "home#about"
